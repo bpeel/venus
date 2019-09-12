642b18ea61b10d7d2ec9e4e76757aee087130633
Neil Roberts <bpeeluk@yahoo.co.uk>
2019-09-12 09:28:52 +0200

    Add the script that forwards to Telegram

---
+++ forward-blogs.py
@@ -0,0 +1,101 @@
+#!/usr/bin/python3
+
+import urllib.request
+import json
+import io
+import html
+import sys
+import time
+import os
+import random
+import re
+import glob
+import xml.etree.ElementTree as ET
+
+class SendMessageException(Exception):
+    pass
+
+def send_message(args):
+    try:
+        req = urllib.request.Request(send_message_url,
+                                     json.dumps(args).encode('utf-8'))
+        req.add_header('Content-Type', 'application/json; charset=utf-8')
+        rep = json.load(io.TextIOWrapper(urllib.request.urlopen(req), 'utf-8'))
+    except urllib.error.URLError as e:
+        raise SendMessageException(e)
+    except json.JSONDecodeError as e:
+        raise SendMessageException(e)
+
+    try:
+        if rep['ok'] is not True:
+            raise SendMessageException("Unexpected response from "
+                                       "sendMessage request")
+    except KeyError as e:
+        raise SendMessageException(e)
+
+def send_entry(title, link):
+    args = {
+        'chat_id' : channel_name,
+        'text' : '<a href="{}">{}</a>'.format(html.escape(link, quote=True),
+                                              html.escape(title)),
+        'parse_mode' : 'HTML'
+    }
+    send_message(args)
+
+def get_link(root):
+    for link in root.findall("./{http://www.w3.org/2005/Atom}link"):
+        rel = link.get("rel", "alternate")
+        if rel != "alternate":
+            continue
+
+        link = link.get("href")
+        if link is None:
+            continue
+
+        return link
+    
+    return None
+
+conf_dir = os.path.expanduser("~/.esperantose")
+apikey_file = os.path.join(conf_dir, "apikey")
+
+with open(apikey_file, 'r', encoding='utf-8') as f:
+    apikey = f.read().rstrip()
+
+urlbase = "https://api.telegram.org/bot" + apikey + "/"
+send_message_url = urlbase + "sendMessage"
+channel_name = "@blogaro"
+
+try:
+    with open(os.path.expanduser("~/.sent-links")) as f:
+        sent_links = set(line.rstrip() for line in f)
+except FileNotFoundError:
+    sent_links = set()
+
+for fn in glob.glob(os.path.expanduser("~/planet/pscache/*")):
+    if not os.path.isfile(fn):
+        continue
+
+    tree = ET.parse(fn)
+    root = tree.getroot()
+
+    link = get_link(root)
+    if link is None:
+        continue
+
+    if link in sent_links:
+        continue
+
+    title = root.find("./{http://www.w3.org/2005/Atom}title")
+    if title is None:
+        continue
+
+    title = "".join(title.itertext())
+
+    send_entry(title, link)
+
+    sent_links.add(link)
+
+with open(os.path.expanduser("~/.sent-links"), "w") as f:
+    for link in sent_links:
+        print(link, file=f)
